9260cf7de2afc54a7cd8dc47481ecfbca00e0175
ec2-user <martinjasonthorpe@gmail.com>
2018-06-27 12:43:55 +0100

    Testing tagging retrieval

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import boto3
 
 # List available waiters
@@ -5,3 +6,34 @@ ec2 = boto3.client('ec2', region_name='eu-west-1')
 print ec2.waiter_names
 
 waiter = ec2.get_waiter('snapshot_completed')
+
+#
+
+instance = ec2.describe_instances(
+	Filters=[
+		{
+			'Name' : 'tag:Backup',
+			'Values' : [
+			'Yes',
+			]
+		}
+	]
+)
+print("RAW instance variable data is %s" % instance)
+print("\n");print("\n");print("\n")
+if "Backup" in instance['Reservations'][0]['Instances'][0]['Tags']:
+	print("Backup stated")
+print("\n");print("\n");print("\n")
+print(instance['Reservations'][0]['Instances'][0]['Tags'])
+print("\n");print("\n");print("\n")
+print(instance['Reservations'][0]['Instances'][0]['Tags'][1])
+tags = instance['Reservations'][0]['Instances'][0]['Tags']
+print("Tags dictionary now contains: %s" % tags)
+listlen = len(tags)
+print("Tags list length is %d" % (listlen))
+dictlen = len(tags[0])
+print("Tags dictionary length is %d" % (dictlen))
+tagsfind = tags[1].get('Backup',"not found")
+print("Looking for specific value, did we find it? %s" % (tagsfind))
+print("\n");print("\n");print("\n")
+print(tags[1])
